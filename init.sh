887bfa75320bc912b017f3aaade1f7554e620d4d
lin <lin@lindeMacBook-Pro.local>
2019-09-16 10:06:59 +0800

    修改路径

--- init.sh
+++ init.sh
@@ -2,20 +2,20 @@
 # 找到新建项目根目录
 cd ../
 pwd [-P]
-cp -r ./vue-scss-js-init/scripts ./
+cp -r ./init-vue-scss-js/scripts ./
 echo "copy 脚本文件成功"
 rm -rf ./src
 echo "delete 新建项目 src 文件成功"
-cp -r ./vue-scss-js-init/src ./
+cp -r ./init-vue-scss-js/src ./
 echo "copy 脚本文件 src 成功"
-cp -r ./vue-scss-js-init/vue.config.js ./
+cp -r ./init-vue-scss-js/vue.config.js ./
 echo "copy 脚本文件 vue.config.js 成功"
 
-if read -t 5 -p "是否删除 vue-scss-js-init 文件?（yes/No）" website
+if read -t 5 -p "是否删除 init-vue-scss-js 文件?（yes/No）" website
 then
-    echo "删除 vue-scss-js-init 文件"
-#    rm -rf ./vue-scss-js-init
+    echo "删除 init-vue-scss-js 文件"
+#    rm -rf ./init-vue-scss-js
 else
-    echo "保留 vue-scss-js-init 文件"
+    echo "保留 init-vue-scss-js 文件"
 fi
 exit 0
